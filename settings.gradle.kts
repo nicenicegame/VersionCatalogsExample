14a7d3fbee77407ef9b7e467b1e94ae121accadc
nicenicegame <nicenicegame@gmail.com>
2023-04-16 22:54:30 +0700

    add default gradle script

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "VersionCatalogsExample"
+include(":androidApp")
+include(":shared")
